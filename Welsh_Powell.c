870514ff0d4308ce4d44731c7792127d5108e9f9
ppelizon <pietrofernandopelizon@gmail.com>
2025-12-03 23:33:51 -0300

    -fix: Vou utilizar qsort da biblioteca padrão de C mesmo + reorganização das funções pra explicação em sala de aula

--- Welsh_Powell.c
+++ Welsh_Powell.c
@@ -22,38 +22,6 @@ typedef struct st_grafo {
     vertice *vertices;
 } grafo;
 
-void insertion_sort(void *base, size_t num_elementos, size_t tamanho_elemento, int (*cmp_data)(const void *a, const void *b)) {
-    if (num_elementos <= 1) {
-        return;
-    }
-
-    char *arr = (char*)base;
-    char *temp_key = malloc(tamanho_elemento);
-    if (temp_key == NULL) {
-        return;
-    }
-    for (size_t i = 1; i < num_elementos; i++) {
-
-        char *ptr_i= arr + i * tamanho_elemento;
-        memcpy(temp_key, ptr_i, tamanho_elemento);
-        char *ptr_j = ptr_i - tamanho_elemento;
-
-        int j = (int)i - 1;
-
-        while (j >= 0 && cmp_data(ptr_j, temp_key) > 0) {
-
-            memmove(ptr_j + tamanho_elemento, ptr_j, tamanho_elemento);
-
-            j--;
-            ptr_j -= tamanho_elemento;
-        }
-
-        char *ptr_insertion = arr + (j + 1) * tamanho_elemento;
-        memcpy(ptr_insertion, temp_key, tamanho_elemento);
-    }
-
-    free(temp_key);
-}
 
 grafo *init_grafo(int n) {
     if (n <= 0) return NULL;
@@ -94,6 +62,24 @@ void add_aresta(const grafo *g, int u, int v) {
     insere_no_inicio(&g -> vertices[v], u);
 }
 
+void liberar_grafo(grafo *g) {
+    if (g == NULL) return;
+
+    for (int i = 0; i < g -> num_vertices; i++) {
+
+        no_adj *atual = g -> vertices[i].inicio;
+
+        while (atual != NULL) {
+            no_adj *aux = atual;
+            atual = atual -> prox;
+            free(aux);
+        }
+    }
+
+    free(g -> vertices);
+    free(g);
+}
+
 bool pode_pintar(const grafo *g, const int u, const char cor_atual) {
     if (g == NULL) return false;
 
@@ -130,17 +116,6 @@ int comparar_graus(const void *a, const void *b) {
     return e2 -> grau - e1 -> grau;
 }
 
-
-/*
-    'A' -> Vermelho
-    'B' -> Verde
-    'C' -> Azul
-    'D' -> Rosa
-    'E' -> Azul Serenity
-    'F' -> Vinho
-    '0' -> Cinza (sem cor)
-*/
-
 // Lista global de cores disponíveis
 char lista_cores[] = {'A', 'B', 'C', 'D', 'E', 'F'};
 
@@ -156,12 +131,13 @@ int Welsh_Powell(const grafo *g) {
         lista_ordenacao[i].grau = g -> vertices[i].grau;
     }
 
-    insertion_sort(lista_ordenacao, n, sizeof(elemento_ordenacao), comparar_graus);
+    qsort(lista_ordenacao, n, sizeof(elemento_ordenacao), comparar_graus);
 
     int cor_atual_index = 0;
     int vertices_coloridos = 0;
 
     while (vertices_coloridos < n) {
+
         char cor_atual = lista_cores[cor_atual_index];
 
         for (int i = 0; i < n; i++) {
@@ -186,7 +162,6 @@ int Welsh_Powell(const grafo *g) {
 void print_resultado(const grafo *g) {
     if (g == NULL) return;
 
-
     printf("\nResultado da coloração:\n");
     for (int i = 0; i < g -> num_vertices; i++) {
         printf("Vertice %d (Grau %d) -> Cor: %c\n",
@@ -194,23 +169,38 @@ void print_resultado(const grafo *g) {
     }
 }
 
-void liberar_grafo(grafo *g) {
-    if (g == NULL) return;
+void gerar_imagem_grafo(grafo *g);
 
-    for (int i = 0; i < g -> num_vertices; i++) {
+int main() {
+    int numero_de_vertices = 12;
+    grafo *g = init_grafo(numero_de_vertices);
 
-        no_adj *atual = g -> vertices[i].inicio;
+    printf("Gerando grafo com %d vertices...\n", numero_de_vertices);
 
-        while (atual != NULL) {
-            no_adj *aux = atual;
-            atual = atual -> prox;
-            free(aux);
-        }
-    }
+    add_aresta(g, 0, 1);
+    add_aresta(g, 0, 2);
+    add_aresta(g, 0, 3);
+    add_aresta(g, 0, 4);
+    add_aresta(g, 4, 5);
+    add_aresta(g, 5, 6);
+    add_aresta(g, 6, 7);
+    add_aresta(g, 6, 8);
+    add_aresta(g, 8, 9);
+    add_aresta(g, 4, 7);
 
-    free(g -> vertices);
-    free(g);
+
+    printf("Executando algoritmo de coloração...\n");
+    int numero_cromatico = Welsh_Powell(g);
+    print_resultado(g);
+    printf("\nNúmero cromático do grafo com %i vértices: %i\n", numero_de_vertices, numero_cromatico);
+
+    gerar_imagem_grafo(g);
+
+    liberar_grafo(g);
+    return 0;
 }
+
+
 
 void gerar_imagem_grafo(grafo *g) {
     if (g == NULL) return;
@@ -240,33 +230,4 @@ void gerar_imagem_grafo(grafo *g) {
     printf("\n[C] Dados exportados. Chamando script Python...\n");
 
     system("python plot_grafo.py");
-}
-
-int main() {
-    int numero_de_vertices = 10;
-    grafo *g = init_grafo(numero_de_vertices);
-
-    printf("Gerando grafo com %d vertices...\n", numero_de_vertices);
-
-    add_aresta(g, 0, 1);
-    add_aresta(g, 0, 2);
-    add_aresta(g, 0, 3);
-    add_aresta(g, 0, 4);
-    add_aresta(g, 4, 5);
-    add_aresta(g, 5, 6);
-    add_aresta(g, 6, 7);
-    add_aresta(g, 6, 8);
-    add_aresta(g, 8, 9);
-    add_aresta(g, 4, 7);
-
-
-    printf("Executando algoritmo de coloração...\n");
-    int numero_cromatico = Welsh_Powell(g);
-    print_resultado(g);
-    printf("\nNúmero cromático do grafo com %i vértices: %i\n", numero_de_vertices, numero_cromatico);
-
-    gerar_imagem_grafo(g);
-
-    liberar_grafo(g);
-    return 0;
 }
